b612ccc4185250bbc58f8c6d45c88b67a476f806
Jkanare <Jyoti.kanare1992@gmail.com>
2024-08-02 11:25:07 +0530

    Javascript File
    Javascript File

---
+++ 01-variable-assignment.js
@@ -0,0 +1,9 @@
+console.log("My city name is: Indore");
+console.log("My best friend name is: Arjun ");
+console.log("My country name is: India");
+console.log("My school name is: Brilliant School");
+console.log("My collage name is: IES, IPS Academy");
+
+
+
+
---
+++ 02-typeof.js
@@ -0,0 +1,36 @@
+console.log("========1st condition======");
+var bankName;
+console.log(bankName);
+var bankNameTypeOf= typeof bankName;
+console.log("Bank name Type of is:", bankNameTypeOf);
+var bankName= 'SBI Bank';
+console.log("Bank name is:", bankName);
+var bankNameTypeOf= typeof bankName;
+console.log("BankName type of is:",bankNameTypeOf);
+
+console.log("========2nd condition======");
+var marks= '90%';
+console.log("Mark is:", marks);
+var marksTypeOf= typeof marks;
+console.log("Marks type of is:", marksTypeOf);
+
+console.log("========3rd condition======");
+var isWorking;
+console.log("Is working:", isWorking);
+var isWorking= true;
+console.log("Is working:", isWorking);
+var isWorkingTypeOf= typeof isWorking;
+console.log("Is working type of is:",isWorkingTypeOf);
+
+console.log("========4th condition Part (a)======");
+var totalCount= "Hundred and Seven";
+console.log("Total count is:",totalCount);
+var totalCountTypeOf= typeof totalCount;
+console.log("Total count type of is:", totalCountTypeOf);
+
+console.log("========4th condition Part (b)======");
+var totalCount= 107;
+console.log("Total count is:",totalCount);
+var totalCountTypeOf= typeof totalCount;
+console.log("Total count type of is:", totalCountTypeOf);
+
