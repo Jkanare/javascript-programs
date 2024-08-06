a319da24e65a069af6a5ce89e862373a397b6df0
Jkanare <Jyoti.kanare1992@gmail.com>
2024-08-06 16:27:07 +0530

    Operator assignment-B
    Operator assignment-B

---
+++ 05-operatorB.js
@@ -0,0 +1,52 @@
+console.log(`1. ================================Find the greater number====================================`);
+
+function greaterNumber(n1, n2) {
+  var result = n1 >= n2 ? n1 : n2;
+  console.log(`Given numbers are ${n1}, ${n2} and the greater number is: ${result}`);
+}
+greaterNumber(10, -10);
+greaterNumber(800, 899);
+
+console.log(`2. ============================Check even or odd number========================================`);
+
+function isEvenOrOddNumber(num){
+    var result = num%2==0 ? true: false;
+     return result;
+}
+var result = isEvenOrOddNumber(29);
+console.log(`Is given number 29 is even?: ${result}`);
+
+var result = isEvenOrOddNumber(44);
+console.log(`Is given number 44 is even?: ${result}`);
+
+var result = isEvenOrOddNumber(0);
+console.log(`Is given number 0 is even?: ${result}`);
+
+var result = isEvenOrOddNumber(101);
+console.log(`Is given number 101 is even?: ${result}`);
+
+console.log(`3. =========================Check even or odd length===========================================`);
+
+function wordLength(sentence){
+console.log(`Given word is: ${sentence}`);
+
+var len = sentence.length;
+console.log(`Given word length is: ${len}`);
+ 
+var result = len%2== 0 ? true: false;
+return result;
+}
+
+var result = wordLength("JavaScript");
+console.log(`Is JavaScript word length is even? : ${result}`);
+
+var result = wordLength("developer");
+console.log(`Is developer word length is even? : ${result}`);
+
+var result = wordLength("Google");
+console.log(`Is Google word length is even? : ${result}`);
+
+
+
+
+
